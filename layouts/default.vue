6b5acfd80815601a26c41a94496c618fc5288b4c
Gürhan Ustali <ustali@ahinur.com>
2023-10-10 10:07:46 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <NuxtLink to="/" class="font-bold">Nuxt App</NuxtLink>
+                <ul class="flex gap-4">
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
+                    <li><NuxtLink to="/products/abc" class="btn">Products ABC</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <div class="container mx-auto p-4">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .router-link-exact-active{
+        color: maroon;
+    }
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Hakkında</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil modi sint dignissimos maxime excepturi soluta quae adipisci aperiam? Dolore hic aliquid quas illo minus eaque animi dolores laboriosam aspernatur quia!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
